1fdc605eaf9f653a9f34e975bba0320c6ee23204
MichaelGranzeier <granzeierpeter@gmail.com>
2022-06-17 13:28:40 -0400

    adding to github

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿// FizzBuzz
+
+Console.WriteLine("FizzBuss");
+
+
+int count = 1;
+
+while (count <= 100)
+{
+    int rem = count % 3;
+    int remTwo = count % 5;
+
+    if (rem == 0 && remTwo == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (rem == 0 && remTwo != 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (remTwo == 0 && rem != 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(count);
+    }
+
+    count++;
+}
